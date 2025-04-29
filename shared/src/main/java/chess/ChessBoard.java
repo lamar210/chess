cdecc4409fe85d4500bdb22a9e1ba88dcb4b56ba
lamar210 <lms210@byu.edu>
2025-04-29 13:43:59 -0600

    Revert "chess board"
    This reverts commit 9a5b6fba7300c03b5388e72bd2029f887b105ed9.

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -1,8 +1,4 @@
 package chess;
-import java.util.Map;
-import java.util.HashMap;
-import java.util.Objects;
-
 
 /**
  * A chessboard that can hold and rearrange chess pieces.
@@ -11,8 +7,6 @@ import java.util.Objects;
  * signature of the existing methods.
  */
 public class ChessBoard {
-
-    private final Map<ChessPosition, ChessPiece> board = new HashMap<>();
 
     public ChessBoard() {
         
@@ -25,8 +19,7 @@ public class ChessBoard {
      * @param piece    the piece to add
      */
     public void addPiece(ChessPosition position, ChessPiece piece) {
-
-        board.put(position, piece);
+        throw new RuntimeException("Not implemented");
     }
 
     /**
@@ -37,8 +30,7 @@ public class ChessBoard {
      * position
      */
     public ChessPiece getPiece(ChessPosition position) {
-
-        return board.get(position);
+        throw new RuntimeException("Not implemented");
     }
 
     /**
@@ -46,70 +38,6 @@ public class ChessBoard {
      * (How the game of chess normally starts)
      */
     public void resetBoard() {
-
-        board.clear();
-
-        //white: non-pawn pieces
-
-        addPiece(new ChessPosition(1, 1),
-                new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.ROOK));
-        addPiece(new ChessPosition(1, 2),
-                new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.KNIGHT));
-        addPiece(new ChessPosition(1, 3),
-                new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.BISHOP));
-        addPiece(new ChessPosition(1, 4),
-                new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.QUEEN));
-        addPiece(new ChessPosition(1, 5),
-                new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.KING));
-        addPiece(new ChessPosition(1, 6),
-                new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.BISHOP));
-        addPiece(new ChessPosition(1, 7),
-                new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.KNIGHT));
-        addPiece(new ChessPosition(1, 8),
-                new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.ROOK));
-
-        //white: pawn pieces
-        for (int c = 1; c <= 8; c++) {
-            addPiece(new ChessPosition(2, c),
-                    new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN));
-        }
-
-        //black: non-pawn pieces
-        addPiece(new ChessPosition(8, 1),
-                new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.ROOK));
-        addPiece(new ChessPosition(8, 2),
-                new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.KNIGHT));
-        addPiece(new ChessPosition(8, 3),
-                new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.BISHOP));
-        addPiece(new ChessPosition(8, 4),
-                new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.QUEEN));
-        addPiece(new ChessPosition(8, 5),
-                new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.KING));
-        addPiece(new ChessPosition(8, 6),
-                new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.BISHOP));
-        addPiece(new ChessPosition(8, 7),
-                new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.KNIGHT));
-        addPiece(new ChessPosition(8, 8),
-                new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.ROOK));
-
-        //black: pawn pieces
-        for (int c = 1; c <= 8; c++) {
-            addPiece(new ChessPosition(7, c),
-                    new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.PAWN));
-        }
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        ChessBoard that = (ChessBoard) o;
-        return Objects.equals(board, that.board);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hashCode(board);
+        throw new RuntimeException("Not implemented");
     }
 }
