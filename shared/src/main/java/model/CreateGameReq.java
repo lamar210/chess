3973af3dfa5b3ce8d697763f93c2792121e7e649
lamar210 <lms210@byu.edu>
2025-05-26 17:50:27 -0600

    moving files + serverfacade

--- shared/src/main/java/model/CreateGameReq.java
+++ shared/src/main/java/model/CreateGameReq.java
@@ -1,4 +1,4 @@
-package service;
+package model;
 
 public record CreateGameReq(
         Integer gameID,
---
+++ shared/src/main/java/model/ListGamesResult.java
@@ -0,0 +1,5 @@
+package model;
+
+import java.util.List;
+
+public record ListGamesResult(List<GameData> games) {}
