4ef93b702fe4ef5b87f30e8d8c00d80521148a28
lamar210 <lms210@byu.edu>
2025-05-26 19:35:22 -0600

    setting up map

--- client/src/main/java/client/ServerFacade.java
+++ client/src/main/java/client/ServerFacade.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 import java.net.URI;
 import java.net.HttpURLConnection;
 import java.net.URISyntaxException;
+import java.util.Map;
 
 
 public class ServerFacade {
@@ -47,6 +48,39 @@ public class ServerFacade {
         }
         http.connect();
         return http;
+    }
+
+    private Map request(String method, String endpoint, String body) {
+        Map result;
+
+        try {
+            HttpURLConnection http = connection(method, endpoint, body);
+            try (var respBody = http.getInputStream();
+                 var reader = new java.io.InputStreamReader(respBody)) {
+                result = new Gson().fromJson(reader, Map.class);
+            }
+        } catch (IOException | URISyntaxException ex) {
+            result = Map.of("Error", ex.getMessage());
+        }
+        return result;
+    }
+
+    public boolean register(String u, String p, String e) {
+        var body = Map.of (
+                "username", u,
+                "password", p,
+                "email", e
+        );
+
+        String jsonBody = new Gson().toJson(body);
+        Map res = request("POST", "/user", jsonBody);
+
+        if (res.containsKey("Error")) {
+            System.out.println("Register failed: " + res.get("Error"));
+            return false;
+        }
+        setAuthToken((String) res.get("authToken"));
+        return true;
     }
 
 }
