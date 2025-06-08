39bd990eec35d797b89ddeca8c68ac79fea6dda4
lamar210 <lms210@byu.edu>
2025-06-08 16:10:59 -0600

    update

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -7,8 +7,7 @@ import server.Server;
 
 public class Main {
     public static void main(String[] args) throws DataAccessException {
-        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        System.out.println("♕ 240 Chess Server: " + piece);
+        System.out.println("♕ 240 Chess Server");
 
         DataAccess dao = new MySqlDataAccess();
         dao.clear();
