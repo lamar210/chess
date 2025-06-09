fb84a5a763c064c55b408e5b39848dfaacfd370f
lamar210 <lms210@byu.edu>
2025-06-08 20:40:46 -0600

    another update

--- client/src/main/java/client/ServerMessageObserver.java
+++
@@ -1,8 +0,0 @@
-package client;
-
-import websocket.messages.ServerMessage;
-
-public interface ServerMessageObserver {
-
-    void notify(ServerMessage message);
-}
--- server/src/main/java/server/WebSocketHandler.java
+++ server/src/main/java/server/WebSocketHandler.java
@@ -189,7 +189,12 @@ public class WebSocketHandler {
         Server.gameDAO.updateGame(updatedData);
 
         String u = Server.authDAO.getAuth(command.getAuthToken()).username();
-        notifyOthers(session, gameData.gameID(), String.format(" %s has resigned", u));
+        String msg = String.format("%s has resigned", u);
+        notifyOthers(session, gameData.gameID(), msg);
+
+        ServerMessage notif = new ServerMessage(ServerMessage.ServerMessageType.NOTIFICATION);
+        notif.setMessage(msg);
+        session.getRemote().sendString(gson.toJson(notif));
     }
 
     private void handleLeave(Session session, UserGameCommand command) throws IOException, DataAccessException {
